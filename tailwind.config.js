800f6e4a865e638b00656a592105749473f00675
raflimaulanayh <personal.rafly@gmail.com>
2023-07-31 19:20:03 +0700

    feat: add navbar-footer & hero section

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,23 @@
-/** @type {import('tailwindcss').Config} */
+const colors = require("tailwindcss/colors");
+
 module.exports = {
   content: ["*.html"],
   theme: {
-    extend: {},
+    colors: {
+      // You may customize your own custom color here
+      primary: "linear-gradient(180deg, #21A2DC 0%, #B221E3 100%)",
+      secondary: "#384348",
+      dark: "#161616",
+      "dark-gray": "#1A1A1A",
+      ...colors,
+    },
+    extend: {
+      fontFamily: {
+        grotesk: ["Space Grotesk", "sans-serif"],
+        poppins: ["Poppins", "sans-serif"],
+      },
+      // if add custom style save here
+    },
   },
   plugins: [],
 };
