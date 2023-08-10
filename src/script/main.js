07a4b5f3545a5a94cd7161985380beb0e56019d6
raflimaulanayh <personal.rafly@gmail.com>
2023-08-10 22:06:26 +0700

    feat: add setup contact form with emailjs

--- src/script/main.js
+++ src/script/main.js
@@ -1,8 +1,18 @@
 const menu = document.querySelector("#menu");
 const menuMobile = document.querySelector("#menuMobile");
+const navOpen = document.querySelector("#nav-open");
+const navClose = document.querySelector("#nav-close");
 const navbarWrapper = document.querySelector("#navbarWrapper");
 const toTop = document.querySelector("#to-top");
+const openPU = document.querySelector("#popup-open");
+const closePU = document.querySelector("#popup-close");
 
+// Popup Close
+closePU.addEventListener("click", () => {
+  openPU.classList.add("hidden");
+});
+
+// Navbar Scroll
 window.onscroll = function () {
   if (window.scrollY > 1) {
     navbarWrapper.classList.add("blur-navbar");
@@ -19,12 +29,17 @@ window.onscroll = function () {
   }
 };
 
+// Hamburger Menu
 menu.addEventListener("click", () => {
   menuMobile.classList.toggle("hidden");
+  navOpen.classList.toggle("hidden");
+  navClose.classList.toggle("hidden");
   navbarWrapper.classList.remove("blur-navbar");
   navbarWrapper.classList.add("bg-dark");
 });
 
 menuMobile.addEventListener("click", () => {
   menuMobile.classList.add("hidden");
+  navOpen.classList.remove("hidden");
+  navClose.classList.add("hidden");
 });
